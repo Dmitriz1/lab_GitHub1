3c09c81a3c743103d537a65036fddc57ff1cb9e8
Dmitriz1 <stud0000264980@study.utmn.ru>
2022-10-29 15:12:31 +0500

    Добавил команду для кнопки кошка

--- Form1.cs
+++ Form1.cs
@@ -11,5 +11,10 @@ namespace WinGitHub
         {
             MessageBox.Show("ֳאג!");
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ּÿף!");
+        }
     }
 }
